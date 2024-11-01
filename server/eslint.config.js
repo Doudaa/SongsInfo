56bb75eebdba34f2a57b8ab5a3942e6715b76938
unknown <omar.dawood246@gmail.com>
2024-11-01 19:55:08 +0200

    Backend server setup completed

---
+++ server/eslint.config.js
@@ -0,0 +1,11 @@
+// eslint.config.js
+module.exports = [
+    {
+      files: ["**/*.js"],
+      rules: {
+        "no-unused-vars": "warn",
+        "semi": ["error", "always"],
+        "quotes": ["error", "double"],
+      },
+    },
+  ];
